3f35b726044db682ef0cadb7e6bfd8ae6f9fa54d
MariJoAP <malfaro@pernixlabs.com>
2018-06-25 14:20:34 -0600

    Integrate new and create actions for porfolios

--- app/controllers/porfolios_controller.rb
+++ app/controllers/porfolios_controller.rb
@@ -2,4 +2,20 @@ class PorfoliosController < ApplicationController
 	def index
 		@porfolio_items = Porfolio.all
 	end
+
+	def new 
+		@porfolio_item = Porfolio.new
+	end
+
+	def create
+    @porfolio_item = Porfolio.new(params.require(:porfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @porfolio_item.save
+        format.html { redirect_to porfolios_path, notice: 'Your Porfolio item is now live' }
+      else
+        format.html { render :new }
+        end
+    end
+  end
 end
