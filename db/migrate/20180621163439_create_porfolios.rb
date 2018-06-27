0b7fd72debd96112d7915489dc519e6f020b88a1
MariJoAP <malfaro@pernixlabs.com>
2018-06-27 08:49:39 -0600

    Integrated friendly routes for blog posts

--- db/migrate/20180621163439_create_porfolios.rb
+++ db/migrate/20180621163439_create_porfolios.rb
@@ -1,4 +1,4 @@
-class CreatePorfolios < ActiveRecord::Migration[5.2]
+class CreatePorfolios < ActiveRecord::Migration[5.1][5.2]
   def change
     create_table :porfolios do |t|
       t.string :title
